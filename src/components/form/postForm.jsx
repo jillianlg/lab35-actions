f94bb6c4e756f975c61670572094fa16b4d6a345
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-17 15:16:11 -0800

    adds dispatch hook to PostForm

--- src/components/form/postForm.jsx
+++ src/components/form/postForm.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from 'react';
+import { createPost } from '../../actions/postActions';
+import { useDispatch } from '../../state/PostProvider';
 
 const PostForm = () => {
+  const dispatch = useDispatch();
+
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
 
   const handleSubmit = event => {
     event.preventDefault();
+
+    dispatch(createPost({ title, body }));
   };
 
   return (
