a7556ce24e0a2190e93a7be4e2781ff6c14a2cab
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-17 13:47:03 -0800

    imports reducer into BlogProvider

--- src/components/state/BlogProvider.jsx
+++ src/components/state/BlogProvider.jsx
@@ -1,9 +1,10 @@
 import React, { createContext, useReducer } from 'react';
+import reducer, { initialState } from '../reducers/blogReducer';
 
 const BlogContext = createContext(null);
 
 export const BlogProvider = ({ children }) => {
-const [state, dispatch] = useReducer();
+const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <BlogContext.Provider value={}>
