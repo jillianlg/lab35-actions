f20bff574e77b47557c73d0332c443b206dcab5b
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-17 14:47:20 -0800

    adds form to PostForm.jsx

---
+++ src/components/form/postForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const PostForm = () => {
+  return (
+    <form>
+      <input type="text" placeholder="post title" value={} onChange />
+      <input type="text" placeholder="add post" value={} onChange />
+      <button>Submit Post</button>
+    </form>
+  )
+};
+
+export default PostForm;
