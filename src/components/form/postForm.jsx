72ed952ed9f59b72b922f3fe561127c4f73ce366
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-17 14:53:20 -0800

    adds state to PostForm

--- src/components/form/postForm.jsx
+++ src/components/form/postForm.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const PostForm = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
   return (
     <form>
-      <input type="text" placeholder="post title" value={} onChange />
-      <input type="text" placeholder="add post" value={} onChange />
+      <input 
+        type="text" 
+        placeholder="post title" 
+        value={title} 
+        onChange={({ target }) => setTitle(target.value)}/>
+      <input 
+        type="text" 
+        placeholder="add post" 
+        value={body} 
+        onChange={({ target }) => setBody(target.value)}/>
       <button>Submit Post</button>
     </form>
-  )
+  );
 };
 
 export default PostForm;
