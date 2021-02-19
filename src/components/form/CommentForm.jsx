5a4ef99be6fdfee437ff6b1d20fef04c13cdab00
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-18 18:56:35 -0800

    adds CommentForm

---
+++ src/components/form/CommentForm.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { createComment } from '../../actions/commentActions';
+import { useDispatch } from 'react-redux';
+
+export default function CommentForm({ commentIndex }) {
+  const dispatch = useDispatch();
+
+  const [comment, setComment] = useState('');
+
+  const submitForm = e => {
+    e.preventDefault();
+
+    dispatch(createComment({ commentIndex, comment }));
+  };
+
+  const updateComment = ({ target }) => {
+    const { value: comment } = target;
+
+    setComment(comment);
+  };
+
+  return (
+    <form onSubmit={submitForm}>
+      <label htmlFor="comment-body">Comment: </label>
+      <input 
+        required
+        id="comment-body"
+        type="text"
+        onChange={updateComment}
+        value={comment}
+      />
+      <button>Comment!</button>
+    </form>
+  );
+}
+
+CommentForm.propTypes = {
+  commentIndex: PropTypes.string.isRequired
+};
